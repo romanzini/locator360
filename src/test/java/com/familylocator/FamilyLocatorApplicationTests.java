bf2780edf0862717c25d1e3bbbedd5d33c97f642
Rodrigo Romanzini <rodrigo.romanzini@gmail.com>
2026-03-09 10:07:31 -0300

    test(auth): add auth controller and app smoke tests

---
+++ src/test/java/com/familylocator/FamilyLocatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.familylocator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class FamilyLocatorApplicationTests {
+
+  @Test
+  void applicationClassExists() {
+    assertNotNull(FamilyLocatorApplication.class);
+  }
+}
